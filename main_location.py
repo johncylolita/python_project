42aa79b1621530d85c347702f2dc0db6013d1578
Johncy Rodrigues <johncylolita@gmail.com>
2024-06-25 13:22:10 +0100

    Modified the main_location file

--- main_location.py
+++ main_location.py
@@ -1,60 +1,106 @@
 import csv
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+
 
 @dataclass
 class Local_Weather:
-    filename: str
-    temperature: list = field(init=False)
-    humidity: list = field(init=False)
-    location: list = field(init=False)
-    new_data: dict = field(init=False)
-
-    def __post_init__(self):
-        self.temperature, self.humidity, self.location = self.file_reader(self.filename)
-        self.new_data = self.group_by(self.temperature, self.humidity, self.location)
+    location: str
+    max_temp: int
+    min_temp: int
+    avg_temp: float
+    avg_humid: float
 
     @classmethod
-    def file_reader(cls, filename):
-        with open(filename, "r") as csvfile:
-            reader = csv.reader(csvfile)
-            next(reader)
-
-            temperature = []
-            humidity = []
-            location = []
-
-            for row in reader:
-                temperature.append(float(row[1]))
-                humidity.append(int(row[2]))
-                location.append(row[3])
-
-        return temperature, humidity, location
-
-    @classmethod
-    def group_by(cls, temperature, humidity,location):
-        from collections import defaultdict
-        new_data = defaultdict(list)
-        for temp, humid, loc in zip(temperature, humidity, location):
-            new_data[loc].append((temp,humid))
-        return new_data
+    def weather_by_location(cls, location, max_temp, min_temp, avg_temp, avg_humid):
+        return cls(
+            location = location,
+            max_temp = max_temp,
+            min_temp = min_temp,
+            avg_temp = avg_temp,
+            avg_humid = avg_humid
+        )
     
-    @staticmethod
-    def average(lst):
-        return sum(lst) / len(lst)
-
-    @classmethod
-    def metrics(cls,new_data):
-        for loc, data in new_data.items():
-            temp = [i for i,j in data]
-            humid = [j for i,j in data]
-            print(f"Location:{loc}")
-            print(f"Maximum temperature: {max(temp)}")
-            print(f"Minimum temperature: {min(temp)}")
-            print(f"Average temperature: {cls.average(temp)}")
-            print(f"Avearge humidity: {cls.average(humid)}")
-            
+    def print_metrics(self):
+        print(f"Location:{self.location}")
+        print(f"Maximum temperature: {self.max_temp}")
+        print(f"Minimum temperature: {self.min_temp}")
+        print(f"Average temperature: {self.avg_temp}")
+        print(f"Avearge humidity: {self.avg_humid}")
 
 
-if __name__ == '__main__':
-    weather_analysis = Local_Weather('weather_data_with_location.csv')
-    Local_Weather.metrics(weather_analysis.new_data)
+def average(lst):
+    return sum(lst) / len(lst)
+
+
+def file_reader(filename):
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+
+        weather_data = {}
+
+        for row in reader:
+            location = row[3]
+            temp = float(row[1])
+            humid = float(row[2])
+
+            if location not in weather_data:
+                weather_data[location] = {'temp': [], 'humid': []}
+
+            weather_data[location]['temp'].append(temp)
+            weather_data[location]['humid'].append(humid)
+
+    return weather_data
+
+def analyze_weather_data(weather_data):
+    weather_metrics = []
+
+    for location, data in weather_data.items():
+        max_temp = max(data['temp'])
+        min_temp = min(data['humid'])
+        avg_temp = average(data['temp'])
+        avg_humid = average(data['humid'])
+
+        weather = Local_Weather.weather_by_location(
+            location,
+            max_temp,
+            min_temp,
+            avg_temp,
+            avg_humid
+        )
+
+        weather_metrics.append(weather)
+
+    return weather_metrics
+
+
+def print_weather_metrics(weather_metrics):
+    for weather in weather_metrics:
+        weather.print_metrics()
+        print()
+
+
+if __name__ == "__main__":
+    weather_data = file_reader("weather_data_with_location.csv")
+    weather_metrics = analyze_weather_data(weather_data)
+    # print(weather_metrics)
+    print_weather_metrics(weather_metrics)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
